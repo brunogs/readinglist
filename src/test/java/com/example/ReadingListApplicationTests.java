ef9224856598dbd8abf953985cc55822ddfbda04
Bruno Silva <bruno.silva@belezanaweb.com.br>
2017-02-13 14:01:03 -0200

    renamed test, fix test

--- src/test/java/com/example/ReadingListApplicationTests.java
+++ src/test/java/com/example/ReadingListApplicationTests.java
@@ -2,12 +2,14 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootConfiguration
 @SpringBootTest
-public class DemoApplicationTests {
+public class ReadingListApplicationTests {
 
 	@Test
 	public void contextLoads() {
